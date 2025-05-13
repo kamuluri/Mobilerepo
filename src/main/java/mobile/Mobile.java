5e3902bb03e4c0acd5d353380cc68efd6b3edd3b
kamuluri <133738856+kamuluri@users.noreply.github.com>
2025-05-13 15:56:40 +0530

    repo commit

--- src/main/java/mobile/Mobile.java
+++ src/main/java/mobile/Mobile.java
@@ -6,6 +6,7 @@ public class Mobile {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
 		System.out.println("hello world2");
+		System.out.println("hello world2");
 	}
 
 }
