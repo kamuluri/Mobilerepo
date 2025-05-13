4ca9987450d579d8b51bcbf0e4a354348861d03f
safiya <kamulurisafiya@gmail.com>
2025-05-13 15:34:42 +0530

    second commit

--- src/main/java/mobile/Mobile.java
+++ src/main/java/mobile/Mobile.java
@@ -4,6 +4,8 @@ public class Mobile {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello world");	}
+		System.out.println("hello world");
+		System.out.println("hello world2");
+	}
 
 }
